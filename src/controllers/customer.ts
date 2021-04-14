3b711abe29787e0bdf3d87f6ed76f77f5a9b305e
Jonatha Priebe <jonatha.priebe@somosiris.com>
2021-04-14 13:55:17 -0300

    create customer controller

---
+++ src/controllers/customer.ts
@@ -0,0 +1,16 @@
+import {Controller, Get} from '@overnightjs/core';
+import { Request, Response } from 'express';
+
+@Controller('customer')
+export class CustomerController {
+  @Get('')
+  public getCustomer(req: Request, res: Response): void {
+    res.status(200).send({
+      name: 'João',
+      gender: 'Masculino',
+      age: 15,
+      dtBirthday: '1988-04-05',
+      idCity: '123'
+    });
+  }
+}
