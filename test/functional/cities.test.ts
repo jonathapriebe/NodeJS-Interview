90642ad4ceca78d21d7b010793b5c34f1f4b0653
Jonatha Priebe <jonatha.priebe@somosiris.com>
2021-04-14 17:36:21 -0300

    add tests for cities

--- test/functional/cities.test.ts
+++ test/functional/cities.test.ts
@@ -1,5 +1,8 @@
+import { City } from "@src/models/city";
+
 describe('Cities functional tests', () => {
-  describe('When creating a newity', () => {
+  beforeAll(async () => await City.deleteMany({}));
+  describe('When creating a new city', () => {
     it('should create a city with success', async() => {
       const newCity = {
         name: 'Porto Alegre',
@@ -10,5 +13,19 @@ describe('Cities functional tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newCity));
     });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newCity = {
+        state: 'Rio Grande do Sul'
+      };
+      const response = await global.testRequest.post('/cities').send(newCity);
+      console.log(response.body);
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'City validation failed: name: Path `name` is required.',
+      });
+    });
+
   });
 });
