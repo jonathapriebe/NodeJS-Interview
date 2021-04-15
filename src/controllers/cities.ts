9ccecc8eb6fcee516e3435accc6a34656ddc0953
Jonatha Priebe <jonatha.priebe@somosiris.com>
2021-04-15 09:13:45 -0300

    create methods of City

--- src/controllers/cities.ts
+++ src/controllers/cities.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Post } from '@overnightjs/core';
 import { City } from '@src/models/city';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
@@ -6,16 +6,45 @@ import mongoose from 'mongoose';
 @Controller('cities')
 export class CitiesController {
   @Post('')
-  public async create(req: Request, res: Response): Promise<void> {
+  public async create(req: Request, res: Response): Promise<Response> {
     try {
       const city = new City(req.body);
       const result = await city.save();
-      res.status(201).send(result);
+      return res.status(201).send(result);
     } catch (error) {
       if (error instanceof mongoose.Error.ValidationError) {
-        res.status(422).send({ error: error.message });
+        return res.status(422).send({ error: error.message });
       } else {
-        res.status(500).send({ error: 'Internal Server Error' });
+        return res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
+  }
+
+  @Get('')
+  public async get(req: Request, res: Response): Promise<Response> {
+    try {
+      const name = req.query.name;
+      const state = req.query.state;
+
+      if (!name && !state) {
+        return res.status(422).send({ error: 'Parameter not found' });
+      }
+
+      let filter = {};
+
+      filter = name ? {...filter, name} : filter;
+      filter = state ? {...filter, state} : filter;
+
+      const result = await City.findOne(filter);
+      if (!result) {
+        return res.status(404).send({ error: 'City not found.' });
+      }
+      return res.status(200).send(result);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        return res.status(422).send({ error: error.message });
+      } else {
+        return res.status(500).send({ error: 'Internal Server Error' });
       }
     }
   }
