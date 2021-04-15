b9e3c24ff705e99f40fa72c70ecaddf316a3ffd3
Jonatha Priebe <jonatha.priebe@somosiris.com>
2021-04-15 09:13:14 -0300

    add tests of City

--- test/functional/cities.test.ts
+++ test/functional/cities.test.ts
@@ -1,7 +1,15 @@
 import { City } from '@src/models/city';
 
 describe('Cities functional tests', () => {
-  beforeAll(async () => await City.deleteMany({}));
+  beforeAll(async () => { 
+    await City.deleteMany({});
+    const city = new City({
+      _id: '607765195331817a34e32c1e',
+      name: 'Porto Alegre',
+      state: 'Rio Grande do Sul',   
+    });
+    await city.save();
+  });
   describe('When creating a new city', () => {
     it('should create a city with success', async () => {
       const newCity = {
@@ -24,6 +32,33 @@ describe('Cities functional tests', () => {
       expect(response.body).toEqual({
         error: 'City validation failed: name: Path `name` is required.',
       });
+    });
+  });
+
+  describe('When find a city', () => {
+    it('Should return a city by name with success', async () => {
+      const { body, status } = await global.testRequest.get('/cities?name=Porto Alegre');
+      const city = {
+        id: '607765195331817a34e32c1e',
+        name: 'Porto Alegre',
+        state: 'Rio Grande do Sul',   
+      };
+      expect(status).toBe(200);
+      expect(body).toEqual(expect.objectContaining(city));
+    });
+
+    it('Should return 422 if not send a parameter', async () => {
+      const { body, status } = await global.testRequest.get('/cities');
+      
+      expect(status).toBe(422);
+      expect(body).toEqual({ error: 'Parameter not found' });
+    });
+
+    it('Should return 404 if not find a city', async () => {
+      const { body, status } = await global.testRequest.get('/cities?name=Pelotas');
+      
+      expect(status).toBe(404);
+      expect(body).toEqual({ error: 'City not found.' });
     });
   });
 });
